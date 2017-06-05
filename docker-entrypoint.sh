887d2de818fe81fb89ed1ffeab4b247ea79c87c5
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-06 01:03:57 +1000

    Add setup-secrets.sh with support of KONG_PG_PASSWORD_FILE

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/usr/local/bin/dumb-init /bin/bash
 set -e
 
+# Read secrets from filesystem and export them as environmnet variables
+exec "/setup-secrets.sh"
+
+# Register kong-admin and kong-dashboard api endpoints
+# and assicotiate consumer and credentials for admin access
+# exec "/setup-admin.sh"
+
 # Disabling nginx daemon mode
 export KONG_NGINX_DAEMON="off"
 

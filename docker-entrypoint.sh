1c28a68980324df8c60aab767bd14a5c4d47fedb
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-06 01:11:55 +1000

    Use file_env function for setup envs from file path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,35 @@
 #!/usr/local/bin/dumb-init /bin/bash
 set -e
 
-# Read secrets from filesystem and export them as environmnet variables
-exec "/setup-secrets.sh"
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'KONG_PG_PASSWORD'
+
+if [[ z - "$KONG_PG_PASSWORD" ]]; then
+  echo >&2 'You need to specify one of KONG_PG_PASSWORD or KONG_PG_PASSWORD_FILE'
+  exit 1
+fi
 
 # Register kong-admin and kong-dashboard api endpoints
 # and assicotiate consumer and credentials for admin access
